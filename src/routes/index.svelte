834d65d2eb5e8c50a309e49a8579049ad953e312
nate <nathan@activetheory.net>
2022-07-12 12:48:47 -0400

    inital commit for testing

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="eventive-login"></div>
+
+<a href="/order"> Go to order page </a>
---
+++ src/routes/order.svelte
@@ -0,0 +1,10 @@
+<script>
+    
+</script>
+
+<div> 
+    <h2>Here is the order now button</h2>
+
+    <div class="eventive-button" data-event="62be2e7801cd5d007deed3fa"></div>
+    <a href="/"> Go to index </a>
+</div>
